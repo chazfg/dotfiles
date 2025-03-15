aab15c164947e427ca9f0beedbbeb9384730f785
CJ <cjgorey132@gmail.com>
2025-03-15 13:17:35 -0500

    mod treesitters

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -10,6 +8,15 @@ return {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
+      "python",
+      "rust",
+      "bash",
+      "html",
+      "json",
+      "sql",
+      "toml",
+      "yaml",
     },
+    auto_install = true,
   },
 }
